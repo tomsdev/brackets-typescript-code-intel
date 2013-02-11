b7f7a33665db3c7bae85dbfa2819dceeb46cc5cb
tomsdev <74759@supinfo.com>
2013-02-10 17:13:21 -0800

    Fix CodeHint plugin to wait for the typescript file to be processed

--- src/extensions/default/TypeScriptCodeIntel/TypeScriptCodeHints/main.js
+++ src/extensions/default/TypeScriptCodeIntel/TypeScriptCodeHints/main.js
@@ -1,29 +1,52 @@
+/*
+ * Copyright (c) 2012 Adobe Systems Incorporated. All rights reserved.
+ *  
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *  
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *  
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ * 
+ */
+
+
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, regexp: true */
 /*global define, brackets, $, window */
-
-//todo: jshint add regex
 
 define(function (require, exports, module) {
 	"use strict";
 
 	var AppInit             = brackets.getModule("utils/AppInit"),
 		CodeHintManager     = brackets.getModule("editor/CodeHintManager"),
-        TypeScriptService       = require("TypeScriptService");
+        Async               = brackets.getModule("utils/Async"),
+        TypeScriptUtils     = require("TypeScriptUtils"),
+        TypeScriptService   = require("TypeScriptService");
+    
+    /*
+     * Get a typescript-code-hint-specific event name
+     */
+    function eventName(name) {
+        var EVENT_TAG = "brackets-typescript-code-hint";
+        return name + "." + EVENT_TAG;
+    }
 
-    // vide ou lettre juste avant curseur?
-    // si vide:
-    //   chercher point direct avant vide
-    //   si point: member & index after point
-    //   sinon: no completion si c des lettres ... noMember & index courant
-    // sinon:
-    //   chercher point avant lettre (peut y avoir du vide avant lettre)
-    //   si point: member & index after point
-    //   sinon: no completion si c des lettres ... noMember & index courant
     function getCompletionInfo(previousCode, endIndex) {
-        var isMember = false;
-        var lastChar = previousCode[endIndex - 1];
-        var match;
-        var currentText = "";
+        var isMember = false,
+            lastChar = previousCode[endIndex - 1],
+            currentText = "",
+            match;
 
         // if last character is empty
         if (lastChar.match(/[ \t\n]/)) {
@@ -31,9 +54,9 @@ define(function (require, exports, module) {
             match = previousCode.match(/[ \t\n]*$/);
         } else {
             // search the last non valid character
-            match = previousCode.match(/[ \t\n]*[0-9a-zA-Z_\$]*$/);
+            match = previousCode.match(/[ \t\n]*[0-9a-z_\$]*$/i);
             // get current typing text
-            currentText = match[0].match(/[0-9A-Za-z_\$]*$/)[0];
+            currentText = match[0].match(/[0-9a-z_\$]*$/i)[0];
         }
 
         var index = endIndex - match[0].length;
@@ -45,24 +68,19 @@ define(function (require, exports, module) {
             index = endIndex;
         }
 
-        var info = {
+        return {
             index: index,
             isMember: isMember,
             currentText: currentText
         };
-
-        //console.log("completionType: ", JSON.stringify(completionType));
-
-        return info;
     }
 
     function getCompletionAtPosition(pos, doc) {
-        var tsDoc = TypeScriptService.get(doc);
-        var index = tsDoc.getIndexFromPos(pos, doc);
-        var textBefore = doc.getRange({line:0, ch:0}, pos);
-        var info = getCompletionInfo(textBefore, index);
-        var completions = tsDoc.getCompletionsAtIndex(info.index, info.isMember);
-
+        var tsDoc = TypeScriptService.get(doc),
+            index = tsDoc.getIndexFromPos(pos, doc),
+            textBefore = doc.getRange({line: 0, ch: 0}, pos),
+            info = getCompletionInfo(textBefore, index),
+            completions = tsDoc.getCompletionsAtIndex(info.index, info.isMember);
         return {
             entries: completions.entries,
             info: info
@@ -104,14 +122,30 @@ define(function (require, exports, module) {
 				(this.secondaryTriggerKeys.indexOf(implicitChar) !== -1);
 		}
 
-		if (result) {
-			console.log("hasHint!");
-		}
-
 		return result;
 	};
 
-	/**
+	TsHints.prototype._getHints = function (implicitChar) {
+        var that = this,
+            completion = getCompletionAtPosition(this.editor.getCursorPos(), this.editor.document);
+
+        // Store the text that was already typed
+        this.currentText = completion.info.currentText;
+
+        var hints = $.map(completion.entries, function (entry) {
+            if (!that.currentText || entry.name.indexOf(that.currentText) === 0) {
+                return entry.name + ': ' + entry.type;
+            }
+        }).sort();
+
+        return {
+            hints: hints,
+            match: that.currentText,
+            selectInitial: true
+        };
+	};
+    
+    /**
 	 * Returns a list of availble CSS protertyname or -value hints if possible for the current
 	 * editor context.
 	 *
@@ -130,27 +164,24 @@ define(function (require, exports, module) {
 	 * 3. a boolean that indicates whether the first result, if one exists, should be
 	 *    selected by default in the hint list window.
 	 */
-	TsHints.prototype.getHints = function (implicitChar) {
-		var that = this,
-            completion = getCompletionAtPosition(this.editor.getCursorPos(), this.editor.document);
-
-        // Store the text that was already typed
-        this.currentText = completion.info.currentText;
-
-		var hints = $.map(completion.entries, function (entry) {
-			if (!that.currentText || entry.name.indexOf(that.currentText) === 0) {
-				return entry.name + ': ' + entry.type;
-			}
-		}).sort();
-
-        // TODO: return a defered?
-
-		return {
-			hints: hints,
-			match: this.currentText,
-			selectInitial: true
-		};
-	};
+    TsHints.prototype.getHints = function (implicitChar) {
+        if (implicitChar) {
+            var result = new $.Deferred(),
+                tsDoc = TypeScriptService.get(this.editor.document);
+            
+            // wait for the current typescript document "change" event because typescript has
+            // to process the file with the changes before getting the completions
+            $(tsDoc).on(eventName("change"), function () {
+                $(tsDoc).off(eventName("change"));
+                var hints = this._getHints(implicitChar);
+                result.resolve(hints);
+            }.bind(this));
+            
+            return result;
+        } else {
+            return this._getHints(implicitChar);
+        }
+    };
 
 	/**
 	 * Inserts a given CSS protertyname or -value hint into the current editor context.
@@ -164,10 +195,9 @@ define(function (require, exports, module) {
 	 */
 	TsHints.prototype.insertHint = function (hint) {
 		var cursor = this.editor.getCursorPos(),
-			keepHints = false;
-
-		// Take only the part of the hint before ':'
-		var text = hint.match(/^[^:]*/)[0];
+			keepHints = false,
+            // Take only the part of the hint before ':'
+            text = hint.match(/^[^:]*/)[0];
 
 		// Subtract the text that was already typed
 		if (this.currentText) {
@@ -176,13 +206,12 @@ define(function (require, exports, module) {
 
 		// Insert the completion at the cursor position
 		this.editor.document.replaceRange(text, cursor);
-
 		return keepHints;
 	};
 
 	AppInit.appReady(function () {
 		var tsHints = new TsHints();
-		CodeHintManager.registerHintProvider(tsHints, ["typescript"], 0);
+		CodeHintManager.registerHintProvider(tsHints, [TypeScriptUtils.MODE_NAME], 0);
 
 		// For unit testing
 		exports.tsHintsProvider = tsHints;
